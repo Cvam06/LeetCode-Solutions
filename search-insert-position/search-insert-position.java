060db943bb28c65d7bed418d8e41a627dd31d607
Cvam06 <cvam612@gmail.com>
2021-09-26 18:18:23 -0400

    Time: 0 ms (100.00%), Space: 38.5 MB (80.86%) - LeetHub

--- search-insert-position/search-insert-position.java
+++ search-insert-position/search-insert-position.java
@@ -3,24 +3,18 @@ class Solution {
         int left = 0;
         int right = nums.length-1;
         
-        int mid;
-        
-        while(left < right){
-            mid = left+(right-left)/2;
+        while(left <= right){
+            int mid = left + (right-left) / 2;
             if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid] < target){
-                    left = mid+1;
-            }else{
-                right = mid-1;
+            else if(target < nums[mid]){
+                right = mid - 1;
+            }
+            else if(target > nums[mid]){
+                left = mid + 1;
             }
         }
-        
-        if(target > nums[left]){
-            return left+1;
-        }
-        return left;
-        
+        return right+1;
     }
 }

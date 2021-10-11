08c0b2361d8dbef8bc2b12f3a2a42b7d87c40e12
Cvam06 <cvam612@gmail.com>
2021-10-11 10:59:53 -0400

    Time: 536 ms (5.00%), Space: 58.7 MB (16.92%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.java
+++ partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,26 +1,33 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
+        int total = 0;
+        for(int num : nums){
+            total = total + num;
+        }
         // to be equal to both sets, the total needs to be even, odd can't be 2*x
-        if(sum%2!=0){
+        if(total%2!=0){
             return false;
         }
         
-        boolean[][] dp = new boolean[nums.length+1][(sum/2)+1];
-        
-        for(int i=0; i<=nums.length; i++){
-            for(int j=0; j<=(sum/2); j++){
-                if(i==0 || j==0){
-                    dp[i][j] = false;
-                } else if(nums[i-1] > j){
-                    dp[i][j] = dp[i-1][j];
-                } else if(nums[i-1] == j){
-                    dp[i][j] = true;
-                } else{
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-            }
-        }
-        return dp[nums.length][sum/2];
+        return canPartition(nums, 0, 0, total, new HashMap<String, Boolean>());
     }
+    
+    public boolean canPartition(int[] nums, int index, int sum, int total, HashMap<String, Boolean> state){
+        String current = index+""+sum;
+        if(state.containsKey(current)){
+            return state.get(current);
+        }
+        if(total/2 == sum){
+            return true;
+        } 
+        if(sum > total/2 || index >= nums.length){
+            return false;
+        }
+        
+        boolean val = canPartition(nums, index+1, sum, total, state) || canPartition(nums, index+1, sum+nums[index], total, state);
+        state.put(current, val);
+        return val;
+    }
+    
+    
 }

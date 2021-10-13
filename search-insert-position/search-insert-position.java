3f99c25dad650debc8054e9b0d8fc122370ce0bd
Cvam06 <cvam612@gmail.com>
2021-10-13 12:36:35 -0400

    Time: 0 ms (100.00%), Space: 40.6 MB (17.35%) - LeetHub

--- search-insert-position/search-insert-position.java
+++ search-insert-position/search-insert-position.java
@@ -1,20 +1,12 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-        
-        while(left <= right){
-            int mid = left + (right-left) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(target < nums[mid]){
-                right = mid - 1;
-            }
-            else if(target > nums[mid]){
-                left = mid + 1;
-            }
+        int pivot, left = 0, right = nums.length - 1;
+        while (left <= right) {
+          pivot = left + (right - left) / 2;
+          if (nums[pivot] == target) return pivot;
+          if (target < nums[pivot]) right = pivot - 1;
+          else left = pivot + 1;
         }
-        return right+1;
+        return left;
     }
 }

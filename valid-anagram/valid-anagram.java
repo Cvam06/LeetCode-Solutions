55ab64950b9e8965974b776d036ee63a316ddf89
Cvam06 <cvam612@gmail.com>
2021-10-20 17:42:13 -0400

    Time: 5 ms (54.30%), Space: 40.2 MB (43.80%) - LeetHub

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -4,11 +4,26 @@ class Solution {
             return false;
         }
         
-        char[] sChar =s.toCharArray();
-        char[] tChar = t.toCharArray();
+//         char[] sChar =s.toCharArray();
+//         char[] tChar = t.toCharArray();
         
-        Arrays.sort(sChar);
-        Arrays.sort(tChar);
-        return Arrays.equals(sChar, tChar);
+//         Arrays.sort(sChar);
+//         Arrays.sort(tChar);
+//         return Arrays.equals(sChar, tChar);
+        
+        char[] count = new char[26];
+        for(int i = 0;  i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for(int i : count){
+            if(i != 0){
+                return false;
+            }
+        }
+        
+        return true;
     }
+    
 }
